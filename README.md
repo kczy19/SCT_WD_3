7894de749734b35ddf984b3f5ecf75b2e295eed8
Kamal <kamal.gehu2021@gmail.com>
2024-09-20 11:16:46 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tic Tac Toe : SkillCraft internship task
+
+# Overview
+This project is an internship task for SkillCraft Technology for web development interns. 
+The goal is to create a tic tac toe website.
+
+# Features
++ **Interactive Game Grid**: A 3x3 grid where players can click to place their Xs and Os. Make sure it's responsive and visually appealing.
++ **Winning Message Display**: When a player wins, a message pops up declaring the winner.
++ **Color Animation for Winning Cells**: Highlight the winning combination of cells with a color animation.
++ **Restart Button**: This button resets the game board.
+
+# Technolgies used
+
++ **HTML**
++ **CSS**
++ **Javascript**
+
+# How to access
+Visit the live site: [Tic Tac Toe](https://Nishuu3164.github.io/SCT_WD_3)
+
+# clone
+```sh
+git clone https://github.com/Nishuu3164/SCT_WD_3.git
---
+++ script.js
@@ -0,0 +1,100 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+let circleTurn;
+
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const restartButton = document.getElementById('restartButton');
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASS);
+        cell.classList.remove('winning-cell'); 
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    const winningCombination = checkWin(currentClass);
+    console.log("Winning Combination: ", winningCombination); // Debug log
+    if (winningCombination) {
+        endGame(false, winningCombination);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw, winningCombination) {
+    console.log("End Game Called - Draw:", draw, " Winning Combination:", winningCombination); // Debug log
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!';
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+        // Highlight the winning cells
+        if (winningCombination) {
+            winningCombination.forEach(index => {
+                cellElements[index].classList.add('winning-cell');
+            });
+        }
+    }
+    winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    return WINNING_COMBINATIONS.find(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
+}
